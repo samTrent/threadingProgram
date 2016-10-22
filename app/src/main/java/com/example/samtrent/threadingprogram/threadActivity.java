d68b5bf43904ac3a312d088f855f70e8daf7b8c0
Sam Trent <SamTrent@Sams-iMac.local>
2016-10-22 14:13:06 -0600

    first upload!

---
+++ app/src/main/java/com/example/samtrent/threadingprogram/threadActivity.java
@@ -0,0 +1,13 @@
+package com.example.samtrent.threadingprogram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class threadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thread);
+    }
+}
